1023bad20353561961ceb91a5f09cb52d51b33e1
Norman <normangoehl@hotmail.de>
2018-09-29 16:25:16 +0200

    OutputRaw, output amplitude to socket

--- colorchord2/OutputRaw.c
+++ colorchord2/OutputRaw.c
@@ -3,10 +3,14 @@
 //XXX TODO Figure out why it STILL fails when going around a loop
 
 #include <stdlib.h>
-#include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
 
 #include "outdrivers.h"
 #include "notefinder.h"
@@ -19,13 +23,12 @@ struct RawDriver
 	int did_init;
 	int total_leds; // don't need that here
 	float satamp; // global Amplification setting.
+
+	int socket;
+	struct sockaddr_in *servaddr;
+	socklen_t addressLength;
 };
 
-
-struct rawOutput{
-	float satamp;
-
-};
 
 // driver update method.
 static void LEDUpdate(void * id, struct NoteFinder*nf)
@@ -46,11 +49,13 @@ static void LEDUpdate(void * id, struct NoteFinder*nf)
 	int i;
 
 	// Begin of Output-Frame
-	printf("frame, totBins: %d \n", totbins);
+	//printf("frame, totBins: %d \n", totbins);
 
 	// output: setamp
 	// output: totBins
 
+	write(led->socket, nf->note_amplitudes, 48);
+	/*
 	for( i = 0; i < totbins; i++ )
 	{
 		// calculate values
@@ -62,11 +67,14 @@ static void LEDUpdate(void * id, struct NoteFinder*nf)
 		// output binPos[i]
 		// output binVals[i]
 		// output binValsQ[i]
-		printf("%f, %f, %f \n",binpos[i], binvals[i], binvalsQ[i]);
-	}
+		//printf("%f, %f, %f \n",binpos[i], binvals[i], binvalsQ[i]);
+		//printf("%f ", nf->note_amplitudes[i]);
 
+	}
+*/
+	printf("\n");
 	// output: totalBinVal
-	printf("totBinVal: %f -Frame END \n\n", totalbinval);
+	//printf("totBinVal: %f -Frame END \n\n", totalbinval);
 	// End Frame
 
 }
@@ -85,6 +93,27 @@ static struct DriverInstances * OutputRaw()
 	struct RawDriver * led = ret->id = malloc( sizeof( struct RawDriver ) );
 	memset( led, 0, sizeof( struct RawDriver ) );
 
+	// create a socket
+	led->socket = socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in localAddress;
+	localAddress.sin_family = AF_INET;
+	localAddress.sin_port = htons(5518);
+	struct hostent *hostinfo;
+	hostinfo = gethostbyname("192.168.1.11");
+	localAddress.sin_addr = *(struct in_addr*) hostinfo->h_addr;
+
+	connect(led->socket, (struct sockaddr*) &localAddress, sizeof(localAddress));
+/*
+	led->servaddr = malloc(sizeof(localAddress));
+	memset( (char*) led->servaddr, 0, sizeof(localAddress));
+
+	led->servaddr->sin_family = AF_INET;
+	led->servaddr->sin_port = htons(1337);
+
+	led->servaddr->sin_addr = *(struct in_addr*) hostinfo->h_addr;
+
+	// Socket successfully stümpered together
+*/
 	ret->Func = LEDUpdate;
 	ret->Params = LEDParams;
 	LEDParams( led );
